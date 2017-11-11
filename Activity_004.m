4f68306830a83363ce3e1090ed268086ab08d888
G. Mizael Mtz Hdz <g.mizael@hotmail.com>
2017-11-11 00:00:16 -0600

    Update Activity_004.m

--- Activity_004.m
+++ Activity_004.m
@@ -40,7 +40,40 @@ norma_2=norm(v,2)                         %4.4 norma 2 del vector 'v'
 string='Exercise 4.5:';
 fprintf('%s\n',string);
 x=linspace(3,17,101)                %4.5 vector desde 3, hasta 17, con 101 elementos
+%input('Press Any Key to Continue');
+
+
+string='Exercise 4.6:';
+fprintf('%s\n',string);
+a=[-2 1 3];
+b=[6 2 5];  
+dot(a,b)                            %4.6 producto vectorial
+cross(a,b)                          %4.6 producto vectorial
+%input('Press Any Key to Continue');
+
+string='Exercise 4.7:';
+fprintf('%s\n',string);
+a=[-4 0 -3];
+w=[2 7 -1];  
+dot(a,w)                            %4.7 producto vectorial
+cross(a,w)                          %4.7 producto vectorial
+%input('Press Any Key to Continue');
+
+
+string='Exercise 4.8:';
+fprintf('%s\n',string);
+A=[pi 0 -2 5; -1 -1 8 2; -2 6 5 0; 9 0 -3 7];
+fprintf('tan(A)\n');
+tan(A)
+%input('Press Any Key to Continue');
+
+
+
+string='Exercise 4.9:';
+fprintf('%s\n',string);
+a=[8 -3 7];
+b=[0 -3 2];
+c=[-4 5 1];
+
 input('Press Any Key to Continue');
-
-
 
